c0acfd56a43731517e27f779c90128ef687e5352
Chris Coyle <chris.coyle@shell.com>
2026-07-23 09:09:29 -0500

    Publish July 23 OSV scheduler update

--- app.js
+++ app.js
@@ -395,7 +395,7 @@ function hideShiftPreview() {
 
 // Static mode: when true, loads data from /data/ folder instead of API
 let staticMode = false;
-const STATIC_DATA_VERSION = '20260723-logistics-meeting';
+const STATIC_DATA_VERSION = '20260723-osv-update';
 
 // Map API endpoints to static JSON files (relative paths for GitHub Pages)
 const STATIC_DATA_MAP = {
